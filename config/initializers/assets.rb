de223a640d3753bed3a2c421a0d5d0f5ef19e1c9
Jones Agyemang <mightyj@hotmail.co.uk>
2016-07-18 16:46:04 +0100

    include vendor sub-directories

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( logo.png background_light.jpg fonts/stylesheet.css base.css)
+
+# add all image subdirectories in vendor folder
+Dir.glob("#{Rails.root}/vendor/assets/images/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
